619b76561f08fd0a643bc3ebc3b50d2f6b20fac3
Brian Litwin <brianlitwin@Brians-MacBook-Pro.local>
2024-03-28 09:58:48 -0400

    feat: add keypoints view

---
+++ api/model.py
@@ -0,0 +1,4 @@
+from onepose import create_model
+
+MODEL_NAME = "ViTPose_huge_simple_coco"
+model = create_model(MODEL_NAME)
--- api/views.py
+++ api/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .model import model
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import numpy as np
+import cv2
+
+@csrf_exempt 
+def img_keypoints(request):
+    if request.method == 'POST':
+        file = request.FILES.get('image')
+        bytes = np.asarray(bytearray(file.read()), dtype=np.uint8)
+        image = cv2.imdecode(bytes, cv2.IMREAD_COLOR)
+        keypoints = model(image)['points'].tolist()
+
+        return JsonResponse({"keypoints": keypoints, 
+                             "height": image.shape[0], 
+                             "width": image.shape[1]})
+    else:
+        return JsonResponse({'error': 'Apparently, only POST requests allowed'})
